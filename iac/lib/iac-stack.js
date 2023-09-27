d3fc31ac731b8c7306d869ae90a237130323109f
Ian Huet <ian@huet.info>
2023-09-27 11:48:59 +0100

    add IAC

---
+++ iac/lib/iac-stack.js
@@ -0,0 +1,26 @@
+const { Stack, Duration } = require('aws-cdk-lib');
+const s3 = require('aws-cdk-lib/aws-s3');
+const ecr = require('aws-cdk-lib/aws-ecr');
+
+class IacStack extends Stack {
+  /**
+   *
+   * @param {Construct} scope
+   * @param {string} id
+   * @param {StackProps=} props
+   */
+  constructor(scope, id, props) {
+    super(scope, id, props);
+
+    new s3.Bucket(this, 'MyFirstBucket', {
+      versioned: true
+    });
+
+    const repository = new ecr.Repository(this, "ecrDevOpsBasics", {
+      repositoryName: "ecr_dev_ops_basics"
+    });
+    repository.addLifecycleRule({ maxImageCount: 6 });
+  }
+}
+
+module.exports = { IacStack }
